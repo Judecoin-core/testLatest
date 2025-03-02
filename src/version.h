9b80814e3fd6d013dca5fdb202f15a0cef6ca893
“ZY” <245657589@qq.com>
2025-03-02 15:05:07 +0800

    base

---
+++ src/version.h
@@ -0,0 +1,7 @@
+#pragma once
+
+extern const char* const JUDE_VERSION_TAG;
+extern const char* const JUDE_VERSION;
+extern const char* const JUDE_RELEASE_NAME;
+extern const char* const JUDE_VERSION_FULL;
+extern const bool JUDE_VERSION_IS_RELEASE;
